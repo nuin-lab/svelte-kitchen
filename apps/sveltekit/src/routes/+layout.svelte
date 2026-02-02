a12d896b792c3e20e7b2ea651455a9caca165dd4
nuin <62213079+nui-n@users.noreply.github.com>
2026-02-02 22:17:09 +0900

    +!: sveltekitを追加

---
+++ apps/sveltekit/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<!-- This Source Code Form is subject to the terms of the Mozilla Public
+   - License, v. 2.0. If a copy of the MPL was not distributed with this
+   - file, You can obtain one at https://mozilla.org/MPL/2.0/. -->
+
+<script lang="ts">
+  // biome-ignore lint/correctness/noUnusedImports: To ignroe false positive linting errors caused by Biome's partial Svelte support.
+  import favicon from '$lib/assets/favicon.svg';
+
+  // biome-ignore lint/correctness/noUnusedVariables: To ignroe false positive linting errors caused by Biome's partial Svelte support.
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
---
+++ apps/sveltekit/vite.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig, mergeConfig } from 'vitest/config';
+import baseViteConfig from '@svelte-kitchen/vite-config';
+
+import { sveltekit } from '@sveltejs/kit/vite';
+
+import { playwright } from '@vitest/browser-playwright';
+
+export default mergeConfig(
+  baseViteConfig,
+  defineConfig({
+    plugins: [sveltekit()],
+
+    test: {
+      expect: { requireAssertions: true },
+
+      projects: [
+        {
+          extends: './vite.config.ts',
+
+          test: {
+            name: 'client',
+
+            browser: {
+              enabled: true,
+              provider: playwright(),
+              instances: [{ browser: 'chromium', headless: true }],
+            },
+
+            include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+            exclude: ['src/lib/server/**'],
+          },
+        },
+
+        {
+          extends: './vite.config.ts',
+
+          test: {
+            name: 'server',
+            environment: 'node',
+            include: ['src/**/*.{test,spec}.{js,ts}'],
+            exclude: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+          },
+        },
+      ],
+    },
+  }),
+);
