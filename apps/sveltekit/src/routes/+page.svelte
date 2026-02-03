950f10ede6edb449f43ae7a818b71fa8a60a2caf
nui-n <62213079+nui-n@users.noreply.github.com>
2026-02-03 14:37:12 +0900

    +!: sveltekitプロジェクトの追加 (#6)
    * +!: sveltekitを追加
    
    * =*: biomeの設定を修正
    
    * +!: サイトルートにsveltekitページへのリンクを追加
    
    * +!: sveltekitパスの解決

---
+++ apps/sveltekit/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<!-- This Source Code Form is subject to the terms of the Mozilla Public
+   - License, v. 2.0. If a copy of the MPL was not distributed with this
+   - file, You can obtain one at https://mozilla.org/MPL/2.0/. -->
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
