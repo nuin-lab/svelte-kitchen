b98064cce4f0f4c9d24aff31f9173c3319ed83ec
nuin <62213079+nui-n@users.noreply.github.com>
2026-02-07 00:21:39 +0900

    *: タイプ名と型の修正

--- apps/sveltekit/src/routes/intercepting-routes/lib/scripts/index.ts
+++ apps/sveltekit/src/routes/intercepting-routes/lib/scripts/index.ts
@@ -4,7 +4,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-type Image = {
+type PhotoData = {
   id: number;
   alt: string;
 };
@@ -13,12 +13,12 @@ const imageList = [
   866, 957, 961, 971, 981, 987, 992, 994, 1002, 1016, 1019, 1021, 1032, 1037, 1041, 1043, 1050, 1055, 1063, 1067,
 ];
 
-export function getAllImages(): Image[] {
+export function getAllImages(): PhotoData[] {
   return imageList.map((imageId) => ({ id: imageId, alt: 'Sample image from Lorem Picsum' }));
 }
 
-export function getImage(id: number): Image | undefined {
+export function getImage(id: number): PhotoData | null {
   const imageId = imageList.at(id);
 
-  return imageId ? { id: imageId, alt: 'Sample image from Lorem Picsum' } : undefined;
+  return imageId ? { id: imageId, alt: 'Sample image from Lorem Picsum' } : null;
 }
