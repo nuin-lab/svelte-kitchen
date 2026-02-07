57042afc6526bda81762ba21e3350c9459133e82
nuin <62213079+nui-n@users.noreply.github.com>
2026-02-07 15:00:10 +0900

    +: 全体に掛かる基本スタイルの追加

--- apps/sveltekit/src/routes/+layout.svelte
+++ apps/sveltekit/src/routes/+layout.svelte
@@ -6,6 +6,7 @@
   // biome-ignore lint/correctness/noUnusedImports: To ignroe false positive linting errors caused by Biome's partial Svelte support.
   import favicon from '$lib/assets/favicon.svg';
 
+  // biome-ignore lint/style/useConst: Intentionally using let for Svelte prop reactivity.
   // biome-ignore lint/correctness/noUnusedVariables: To ignroe false positive linting errors caused by Biome's partial Svelte support.
   let { children } = $props();
 
@@ -14,7 +15,39 @@
 </script>
 
 <svelte:head>
-  <link rel="icon" href={favicon} />
+  <link rel="icon" href={favicon}>
 </svelte:head>
 
 {@render children()}
+
+<style>
+  :root {
+    --black: hsl(16, 24%, 16%);
+    --white: hsl(16, 8%, 92%);
+
+    --bdr-wd: 1px;
+    --bdr-rds: 0.5rem;
+
+    font-size: max(1rem, max(18px, 0.9375vw));
+  }
+
+  :global {
+    body {
+      background-color: var(--white);
+      color: var(--black);
+
+      font-family: "Archivo", sans-serif;
+    }
+
+    h1 {
+      font-weight: 300;
+      line-height: 1;
+    }
+
+    p {
+      font-family: "IBM Plex Sans JP", sans-serif;
+
+      opacity: 0.75;
+    }
+  }
+</style>
