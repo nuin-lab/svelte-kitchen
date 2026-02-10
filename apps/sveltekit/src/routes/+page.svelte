e8f5584d4dd8bc609af06cd228f1fcd4c5d2c1be
nuin <62213079+nui-n@users.noreply.github.com>
2026-02-10 11:52:52 +0900

    -: ルートページのコンテンツを削除

--- apps/sveltekit/src/routes/+page.svelte
+++ apps/sveltekit/src/routes/+page.svelte
@@ -2,5 +2,5 @@
    - License, v. 2.0. If a copy of the MPL was not distributed with this
    - file, You can obtain one at https://mozilla.org/MPL/2.0/. -->
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
